f6576407fb190b88d0c8e2c9ba412148310e32d6
atikdev-bd <atikdev.bd@gmail.com>
2022-12-28 01:56:29 +0600

    daly task data store mongodb using post method

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-const express = require('express');
-const cors = require('cors');
-const app = express()
-const port = process.env.PORT || 5000
-
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
 
 /// middle were ///
-require('dotenv').config()
-app.use(cors())
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USERS}:${process.env.PASSWORD}@cluster0.h7epoo8.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const tasksCollection = client.db("toDoList").collection("taskData");
+
+    app.post("/task", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const taskResult = await tasksCollection.insertOne(data);
+      res.send(taskResult);
+    });
+  } finally {
 
 
-app.get('/', (req , res)=>{
-    res.send('to-do-server is running')
+  }
+};
 
-})
+run().then((error) => {
+  
+});
 
-app.listen(port,()=>{
-    console.log('server is running on port', port);
-})
+app.get("/", (req, res) => {
+  res.send("to-do-server is running");
+});
+
+app.listen(port, () => {
+  console.log("server is running on port", port);
+});
